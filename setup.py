e6b30ae0b1bdaf5d454bc49b5f87b50f2f901c08
Sean Cronin <seancron@gmail.com>
2015-02-08 23:26:28 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='bender',
+
+    version='0.1.0',
+
+    description="Python chat bot framework",
+    url='https://github.com/seancron/bender',
+
+    author='Sean Cronin',
+    author_email='seancron@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='slack chat api',
+
+    packages=['bender'],
+    install_requires=['slack-rtm', 'blinker>=1.3'],
+    dependency_links=[
+        'git+https://github.com/seancron/slack-rtm.git'
+    ]
+)
